23f3b60c5e103e6a695eec71ae443981e11a277c
SreeNitya <nitya.kf512@gmail.com>
2016-11-10 17:26:03 -0600

    Add files via upload

---
+++ StaticExample.java
@@ -0,0 +1,32 @@
+
+	public class StaticExample 
+	{
+	       
+	        public static void main(String[] args) 
+	        {
+	               
+	                ObjectCounter object1 = new ObjectCounter();
+	                System.out.println(object1.getNumberOfObjects());
+	               
+	                ObjectCounter object2 = new ObjectCounter();
+	                System.out.println(object2.getNumberOfObjects());
+	                               
+	        }
+	}
+	 
+	class ObjectCounter{
+		static int counter=0;
+		       
+	public ObjectCounter()
+	{
+		counter++;
+	}
+			       	       
+		//returns number of objects created till now
+	public int getNumberOfObjects()
+	{
+	 return counter;
+ 	}
+
+
+}
